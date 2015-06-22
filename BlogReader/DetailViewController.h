b5d0a37b494087675f9ce4b090d5c79fca7aa945
Alp Eren Can <alperenc@gmail.com>
2015-06-22 14:36:31 +0300

    Initial Commit

---
+++ BlogReader/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  BlogReader
+//
+//  Created by Alp Eren Can on 22/06/15.
+//  Copyright (c) 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ BlogReader/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  BlogReader
+//
+//  Created by Alp Eren Can on 22/06/15.
+//  Copyright (c) 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
